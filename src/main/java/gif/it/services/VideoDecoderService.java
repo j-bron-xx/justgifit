73f54bb4f4707fb8ef290ead57c9f0a7b66ced56
Jan Broniowski <jan.broniowski@contractors.roche.com>
2017-02-15 19:33:21 +0100

    service layer

--- src/main/java/gif/it/services/VideoDecoderService.java
+++ src/main/java/gif/it/services/VideoDecoderService.java
@@ -1,7 +1,17 @@
 package gif.it.services;
 
-/**
- * Created by broniowj on 2017-02-14.
- */
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.FrameGrabber;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+
+@Service
 public class VideoDecoderService {
+
+    public FFmpegFrameGrabber read(File video) throws FrameGrabber.Exception {
+        FFmpegFrameGrabber frameGrabber = new FFmpegFrameGrabber(video);
+        frameGrabber.start();
+        return frameGrabber;
+    }
 }
